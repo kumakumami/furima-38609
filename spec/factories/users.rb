8a28904f3d4ba55737b82e7c8af26cbb12f7f171
kumakumami <punipunimikan.mk@gmail.com>
2022-10-31 21:26:27 +0900

    テストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
+    nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {'1a'+Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name {"山田"}
+    first_name_kana {"ヤマダ"}
+    last_name {"太郎"}
+    last_name_kana {"タロウ"}
+    birthday {"2000-01-01"}
   end
 end
+
