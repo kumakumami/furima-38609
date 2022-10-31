cb63b993e57e497b755579d09ffd65512827c897
kumakumami <punipunimikan.mk@gmail.com>
2022-10-31 11:34:36 +0900

    バリデーション、パラムスの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
 
   private
@@ -9,4 +11,14 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+  
+  def configure_permitted_parameters
+    added_attrs = [ :nickname, :last_name, :first_name, :last_name_kana,:first_name_kana,:birthday]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
+
+
+
+
+
 end
