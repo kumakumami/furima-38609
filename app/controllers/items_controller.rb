767602d9eea70b910043c75ddbeb9b5a717d29ad
kumakumami <punipunimikan.mk@gmail.com>
2022-11-01 16:19:16 +0900

    アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,29 @@
 class ItemsController < ApplicationController
   def index
+
   end
+  
+  def new
+    @item=Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:neme,:information,:condition_id,:category_id,:state_id,:shipping_id,:delivery_days_id,:item_price).merge(user_id: current_user.id)
+  end
+  
+
 end
