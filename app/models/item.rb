7b00bfc11ee3ff9ed1636fa3e9d17370918a824f
kumakumami <punipunimikan.mk@gmail.com>
2022-11-01 20:25:16 +0900

    価格のバリデーション設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,11 @@ class Item < ApplicationRecord
   
   validates :name, presence: true
   validates :information, presence: true
-  validates :item_price, presence: true
-
+  validates :item_price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :item_price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :image, presence: true
+  
 
 has_one_attached :image
 belongs_to :user
